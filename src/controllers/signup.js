f9c1d62f4f658299c11c7c4b1f31f3d7d0bf9e98
irisakellia <149322062+irisakellia@users.noreply.github.com>
2024-06-11 09:53:47 +0200

    [changed file] to jwt

--- src/controllers/signup.js
+++ src/controllers/signup.js
@@ -1,17 +1,19 @@
 const userService = require("../services/signup");
 
+
 async function createUser(req,res){
     try {
         
         const {name, email, password} = req.body;
         const user = await userService.createUser({name, email, password});
-        res.status(200).json({user:user, message:"user been created successfully"});
+        res.status(200).json({success:true , message:"user been created successfully"});
 
     } catch (error) {
-        res.status(400).json({ message:" can not create user"});
+        res.status(400).json({success:false, message:" can not create user"});
     }
 }
 
 module.exports = {createUser}
+
 
 
