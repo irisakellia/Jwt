728ae2752d78acbf57252d460dbcc23b95cebed2
irisakellia <149322062+irisakellia@users.noreply.github.com>
2024-07-03 16:52:16 +0200

    [token +]

---
+++ configuration/jwtConfig.js
@@ -0,0 +1,8 @@
+const crypto = require('crypto');
+
+
+const secretkey = crypto.randomBytes(32).toString('hex');
+
+module.exports = {
+    secretkey: secretkey
+};
---
+++ utils/jwtUtils.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+const {secretkey} = require('../configuration/jwtConfig')
+
+
+function generateToken(user){
+    const payload = {
+        id:user._id,
+        email:user.email,
+        role:user.role
+    }
+    return jwt.sign(payload, secretkey, {expireIn:"1h"});
+
+    
+}
+
+module.exports = {
+    generateToken
+};
