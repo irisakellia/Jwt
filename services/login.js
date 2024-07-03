05aa1b9783a24ca2339614d66a8e21152f7a06df
irisakellia <149322062+irisakellia@users.noreply.github.com>
2024-07-03 17:05:04 +0200

    [token ++]

---
+++ services/login.js
@@ -0,0 +1,30 @@
+const bcrypt = require('bcrypt');
+const User = require('../models/User')
+const {generateToken} = require('../utils/jwtUtils')
+
+
+async function login(email,password){
+
+    try {
+        const existingUser = await User.findOne({email});
+        if(!existingUser){
+            throw new Error("user not found");
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, existingUser.password);
+        if(!isPasswordValid){
+            throw new Error("Incorrect password");
+        }
+
+        const token = generateToken(existingUser);
+        return token ;
+        
+    } catch (error) {
+        throw new Error("Invalid credentials")
+    }
+
+}
+
+module.exports = {
+    login
+}
