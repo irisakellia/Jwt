982765658e9c6b1aede70785450f17a1fde50f96
irisakellia <149322062+irisakellia@users.noreply.github.com>
2024-07-07 11:22:32 +0200

    [added to jwt]

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const dbConnection = require("./configuration/dbConfig");
 const cors = require ("cors");
 const createAdminAccount = require("./scripts/admin")
 const loginRoute = require('./routes/login')
+const userRoute = require('./routes/user')
 
 const app = express();
 const PORT = 5001;
@@ -17,6 +18,7 @@ app.use(cors());
 dbConnection();
 app.use("/user", signupRoute);
 app.use("/auth",loginRoute);
+app.use("/api",userRoute);
 
 app.listen(PORT, () => {
   console.log(`server is running on : http://localhost:${PORT}`);
